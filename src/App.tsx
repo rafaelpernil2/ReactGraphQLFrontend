f72cf177b249db515a278f32f9a4e126a12977d1
rafaelpernil2 <mysterysynthman@gmail.com>
2021-08-22 00:47:50 +0200

    feat: Added Kubernetes deployment configuration

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import CounterParent from './components/CounterParent';
 function App() {
   return (
     <div className="App">
+      <p>Hello from React, you have deployed this app successfully! :D</p>
       <CounterParent></CounterParent>
     </div>
   );
